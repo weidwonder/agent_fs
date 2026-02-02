1a5336eee0dbf2a8bf4593b72ef876094288354d
weidwonder <weidwonder@gmail.com>
2026-02-02 23:02:24 +0800

    feat(search): export BM25 module

--- packages/search/src/index.ts
+++ packages/search/src/index.ts
@@ -2,4 +2,17 @@
 export const VERSION = '0.1.0';
 
 // BM25
-export { BM25Index } from './bm25';
+export {
+  BM25Index,
+  tokenize,
+  termFrequency,
+  saveIndex,
+  loadIndex,
+  indexExists,
+  bm25Score,
+  DEFAULT_BM25_PARAMS,
+  type BM25IndexOptions,
+  type BM25SearchOptions,
+  type TokenizeOptions,
+  type BM25Params,
+} from './bm25';
